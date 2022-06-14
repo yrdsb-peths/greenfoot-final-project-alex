c00d0dd46c69d7b64305a334ef4075ee432881f8
Alex V <99669753+335744348@users.noreply.github.com>
2022-06-14 10:26:32 -0400

    Found more suitable background, added a play again button to the game over screen

--- GameOverWorld.java
+++ GameOverWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GameOverWorld here.
+ * Screen that pops up when the player has died. Displays an option to play again, and player score.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Alex V.
+ * @version 6.14.2022
  */
 public class GameOverWorld extends World
 {
@@ -13,16 +13,20 @@ public class GameOverWorld extends World
      * Constructor for objects of class GameOverWorld.
      * 
      */
+    Label startOver = new Label("PLAY AGAIN", 50);
+    Button startOverB = new Button(60, 300);
+    
     public GameOverWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 500, 1); 
         Label died = new Label("YOU DIED", 70);
-        addObject(died, 400,250);
-        Label startOver = new Label("Press Enter to start again", 40);
-        addObject(startOver, 400,360);
+        addObject(died, 400,230);
         Label score = new Label("SCORE: " + MyWorld.score, 50);
-        addObject(score, 400, 310);
+        addObject(score, 400, 290);
+        addObject(startOverB, 400, 360);
+        addObject(startOver, 400,360);
+        
         PlayerCharDeath p = new PlayerCharDeath();
         addObject(p, MyWorld.x, MyWorld.y);
     }
@@ -32,6 +36,11 @@ public class GameOverWorld extends World
         if (Greenfoot.isKeyDown("enter"))
         {
             Greenfoot.setWorld(new TitleScreen());
+        }
+        if (Greenfoot.mousePressed(startOverB) || Greenfoot.mousePressed(startOver))
+        {
+            TitleScreen world = new TitleScreen();
+            Greenfoot.setWorld(world);
         }
     }
 }
